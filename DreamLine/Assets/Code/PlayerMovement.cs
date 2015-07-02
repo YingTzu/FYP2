5ec4e0b0568d94feb7843bd3f04f4bb9835aacbf
WANWENN <w-anwen@hotmail.com>
2015-07-02 23:58:01 +0800

    ALOT of Changesss :heart:
    :santa:

--- DreamLine/Assets/Code/PlayerMovement.cs
+++ DreamLine/Assets/Code/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
 	public GameObject player2;
 	public GUITexture potion1;
 	public GUIText debug;
+		
+	private bool mousedown = false;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -16,8 +19,38 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		Touch[] myTouches = Input.touches;
+		#if UNITY_EDITOR
+		MouseInput();
+		#else
+		Touch();
+		#endif
 
+
+	}
+
+	void MouseInput()
+	{
+		if (mousedown == false)
+		{
+			if (Input.GetMouseButton(0) == true)
+			{
+				//mouse input
+			}
+			
+			mousedown = true;
+		}
+		
+		if (Input.GetMouseButton(0) == false && mousedown == true)
+		{
+			mousedown = false;
+		}
+
+	}
+
+	void Touch()
+	{
+		Touch[] myTouches = Input.touches;
+		
 		for(int i = 0; i < Input.touchCount; i++)
 		{
 			if(myTouches[i].position.y > (Screen.height)/2)
@@ -29,26 +62,27 @@ public class PlayerMovement : MonoBehaviour
 				player2.rigidbody2D.AddForce(new Vector2(0, Up));
 			}
 		}
-//		if (Input.touchCount > 0) 
-//		{
-//			Touch touch = Input.GetTouch(0);
-//			int tapCount = Input.touchCount;
-//			for(int i = 0; tapCount > i; i++) 
-//			{    
-//				if(i < 1) 
-//				{
-//					touch = Input.GetTouch(i);
-//				}
-//				if(touch.phase == TouchPhase.Stationary) 
-//				{
-//					if(potion1.GetComponent<GUITexture>().HitTest(touch.position) == true)
-//						debug.text = "powerUp";
-//					if(touch.position.y > (Screen.height)/2)
-//						player.rigidbody2D.AddForce(new Vector2(0, Up));
-//					else if(touch.position.y < (Screen.height)/2)
-//						player2.rigidbody2D.AddForce(new Vector2(0, Up));
-//				}
-//			}
-//		}
+		//		if (Input.touchCount > 0) 
+		//		{
+		//			Touch touch = Input.GetTouch(0);
+		//			int tapCount = Input.touchCount;
+		//			for(int i = 0; tapCount > i; i++) 
+		//			{    
+		//				if(i < 1) 
+		//				{
+		//					touch = Input.GetTouch(i);
+		//				}
+		//				if(touch.phase == TouchPhase.Stationary) 
+		//				{
+		//					if(potion1.GetComponent<GUITexture>().HitTest(touch.position) == true)
+		//						debug.text = "powerUp";
+		//					if(touch.position.y > (Screen.height)/2)
+		//						player.rigidbody2D.AddForce(new Vector2(0, Up));
+		//					else if(touch.position.y < (Screen.height)/2)
+		//						player2.rigidbody2D.AddForce(new Vector2(0, Up));
+		//				}
+		//			}
+		//		}
+
 	}
 }
---
+++ DreamLine/Assets/Code/playerControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+
+	public float movementSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 newVelocity = rigidbody2D.velocity;
+		newVelocity.x = movementSpeed;
+		rigidbody2D.velocity = newVelocity;
+	}
+}
