fefec0bf9090e32fb0f00c9157469da0688e43b1
WANWENN <w-anwen@hotmail.com>
2015-07-07 10:18:06 +0800

    NEWWWWW :3
    :cat: :+1:

---
+++ DreamLine/Assets/Code/cloudScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cloudScript : MonoBehaviour {
+
+	public Sprite thunderClouds;    
+	public Sprite darkClouds;
+
+	public float interval = 0.5f; 
+
+	private bool Thunder = true;    
+	private float timeUntilNextToggle;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		timeUntilNextToggle = interval;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		timeUntilNextToggle -= Time.fixedDeltaTime;
+
+		if (timeUntilNextToggle <= 0) 
+		{
+			Thunder = !Thunder;
+
+			collider2D.enabled = Thunder;
+
+			SpriteRenderer spriteRenderer = ((SpriteRenderer)this.renderer);
+			if (Thunder)
+			{
+				spriteRenderer.sprite = thunderClouds;
+			}
+			else
+			{
+				spriteRenderer.sprite = darkClouds;
+			}
+
+			timeUntilNextToggle = interval;
+		}
+
+	}
+}
---
+++ DreamLine/Assets/Code/followCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followCamera : MonoBehaviour {
+
+	public GameObject target;
+	private float distanceToTarget;
+
+	// Use this for initialization
+	void Start () {
+
+		distanceToTarget = transform.position.x - target.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float targetObjectX = target.transform.position.x;
+		
+		Vector3 newCameraPosition = transform.position;
+		newCameraPosition.x = targetObjectX + distanceToTarget;
+		transform.position = newCameraPosition;    	
+	}
+}
