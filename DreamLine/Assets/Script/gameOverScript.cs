cea9381e97ad0437329d39500a34d1fde2000983
WANWENN <w-anwen@hotmail.com>
2015-07-28 11:01:44 +0800

    :school: :-1:

---
+++ DreamLine/Assets/Script/gameOverScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameOverScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public void Restart()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void Menu()
+	{
+		Application.LoadLevel (0);
+	}
+}
