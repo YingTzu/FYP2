70dd09a043b0170e6d94cd1e6adadc81c44d239c
YingTzu <tzu_0223@hotmail.com>
2015-08-03 15:00:36 +0800

    HighScore

--- DreamLine/Assets/Script/Score.cs
+++ DreamLine/Assets/Script/Score.cs
@@ -7,17 +7,42 @@ public class Score : MonoBehaviour
 	public static int distance;
 	public int latestDistance;
 	public Image Heart;
-	void Start () {
+	public Text playerScore;
+	public Text highScore;
+	void Start () 
+	{
+		if(Application.loadedLevelName == "Endless")
+			distance = 0;
 	}
 
-	void Update () {
-		if (Heart.fillAmount == 0) 
+	void Update () 
+	{
+		if(Application.loadedLevelName == "Endless")
 		{
-			latestDistance = distance;
+			if (Heart.fillAmount == 0) 
+			{
+				latestDistance = distance;
+				if(distance > PlayerPrefs.GetInt("HighScore"))
+					PlayerPrefs.SetInt("HighScore", latestDistance);
+			}
+			else{
+				if(playerControl.movementSpeed == 3)
+				{
+					distance += 1;
+				}
+				else if(playerControl.movementSpeed == 6)
+				{
+					distance +=2;
+				}
+			}
+			this.gameObject.GetComponent<Text>().text = "Score:" + distance;
 		}
-		else{
-			distance++;
-			this.gameObject.GetComponent<Text>().text = "Score: " + distance;
+
+
+		if(Application.loadedLevelName == "Gameover")
+		{
+			playerScore.text = "Your Score: " + distance;
+			highScore.text = "HighScore: " + PlayerPrefs.GetInt("HighScore");
 		}
 	}
 }
